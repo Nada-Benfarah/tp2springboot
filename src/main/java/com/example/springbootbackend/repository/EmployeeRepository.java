b858a23652f09c6364b85b8f3b4a343289294cb0
Nada Benfarah <nadabenfarah3@gmail.com>
2023-10-21 14:26:00 +0200

    first commit

---
+++ src/main/java/com/example/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,6 @@
+package com.example.springbootbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.springbootbackend.model.Employee;
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+}
